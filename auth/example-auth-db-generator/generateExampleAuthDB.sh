591f3f981ef8968073046d9a209b59439f88f3e9
Hokeun Kim <hokeunkim@eecs.berkeley.edu>
2016-09-02 15:33:19 -0700

    changed command for building example-auth-db-generator

--- auth/example-auth-db-generator/generateExampleAuthDB.sh
+++ auth/example-auth-db-generator/generateExampleAuthDB.sh
@@ -5,7 +5,9 @@ if [ ! -d "../databases/auth101" ];
 then
     echo "need to create databases"
 else
-    mvn clean install
+	cd ../
+	mvn -pl example-auth-db-generator -am install
+	cd example-auth-db-generator
     cp target/init-example-auth-db-jar-with-dependencies.jar ../
     cd ../
     java -jar init-example-auth-db-jar-with-dependencies.jar
