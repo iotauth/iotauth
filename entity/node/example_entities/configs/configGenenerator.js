5ee9869b9aa3521eb7a3629bad73c7d5d534e437
Hokeun Kim <hokeunkim@eecs.berkeley.edu>
2017-02-04 17:11:14 -0800

    a new set of entity configs generated from a script

--- entity/node/example_entities/configs/configGenenerator.js
+++ entity/node/example_entities/configs/configGenenerator.js
@@ -21,28 +21,11 @@
 "use strict";
 
 var fs = require('fs');
+var JSON2 = require('JSON2');
 
-var strConfig = fs.readFileSync('template.config', 'utf8');
-var jsonConfig = JSON.parse(strConfig);
 
-console.log(jsonConfig);
 
-function getAuthInfo(id, protocol) {
-    var authInfo = {};
-    authInfo.id = id;
-    authInfo.host = 'localhost';
-    if (id == 101) {
-        authInfo.port = 21900;
-    }
-    else if (id == 102) {
-        authInfo.port = 22900;
-    }
-    if (protocol == 'UDP') {
-        authInfo.port += 2; 
-    }
-    authInfo.publicKey = '../../auth_certs/Auth' + id + 'EntityCert.pem';
-    return authInfo;
-}
+
 /*
 var tcpServerInfoList = [
     { name: 'net1.server', host: 'localhost', port: 21100 },
@@ -75,18 +58,18 @@ var serverList = [
     { name: 'rcServer', port: 300 },
     { name: 'udpServer', port: 400 },
     { name: 'safetyCriticalServer', port: 500 },
-    { name: 'rcUdpServer', port: 600 }];
+    { name: 'rcUdpServer', port: 600 }
+];
 
 var clientList = [
     { name: 'client' },
-//    { name: 'ptClient', port: 200 },
+    { name: 'ptClient' },
     { name: 'rcClient' },
     { name: 'udpClient' },
     { name: 'safetyCriticalClient' },
-    { name: 'rcUdpClient' }];
+    { name: 'rcUdpClient' }
 ];
  
-console.log(getAuthInfo(102, 'TCP'));
 
 // if str.toLowerCase().includes('pt')
 // just add to the server info, don't generate the file
@@ -99,12 +82,180 @@ var tcpServerInfoList = [];
 var udpServerInfoList = [];
 var safetyCriticalServerInfoList = [];
 
+function capitalizeFirstLetter(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
 
-    for (var i = 0; i < fileLines.length; i++) {
-        var line = fileLines[i].trim();
-        if (line.startsWith('//') || line.length == 0) {
-            continue;
-        }
-        fileString += line;
+function getNetName(netId) {
+    return 'net' + netId;
+}
+
+function getKeyPath(netId, entityName, keySuffix) {
+    return '../../credentials/keys/' + getNetName(netId) + '/'
+        + capitalizeFirstLetter(entityName) + keySuffix;
+}
+
+function getEntityInfo(netId, entityName) {
+    var entityInfo = {};
+    entityInfo.name = getNetName(netId) + '.' + entityName;
+    if (entityName.toLowerCase().includes('client')) {
+        entityInfo.group = 'Clients';
     }
+    else if (entityName.toLowerCase().includes('server')) {
+        entityInfo.group = 'Servers';
+    }
+    if (entityName.toLowerCase().includes('udp')) {
+        entityInfo.distProtocol = 'UDP';
+    }
+    else {
+        entityInfo.distProtocol = 'TCP';
+    }
+    if (entityName.toLowerCase().includes('rc')) {
+        entityInfo.usePermanentDistKey = true;
+        entityInfo.permanentDistKey = {
+            'cipherKey': getKeyPath(netId, entityName, 'CipherKey.key'),
+            'macKey': getKeyPath(netId, entityName, 'MacKey.key'),
+            'validity': '365*day'
+        }
+    }
+    else {
+        entityInfo.usePermanentDistKey = false;
+    }
+    entityInfo.privateKey = getKeyPath(netId, entityName, 'Key.pem');
+    return entityInfo;
+}
+
+function getNetPortBase(netId) {
+    return 20000 + netId * 1000;
+}
+
+function getAuthInfo(netId, entityName) {
+    var authInfo = {};
+    authInfo.id = 100 + netId;
+    authInfo.host = 'localhost';
+    authInfo.port = getNetPortBase(netId) + 900;
+    if (entityName.toLowerCase().includes('udp')) {
+        authInfo.port += 2;
+    }
+    if (!entityName.toLowerCase().includes('rc')) {
+        authInfo.publicKey = '../../auth_certs/Auth' + authInfo.id + 'EntityCert.pem';
+    }
+    return authInfo;
+}
+
+function getCryptoInfo(entityName) {
+    var cryptoInfo = {};
+    if (!entityName.toLowerCase().includes('rc')) {
+        cryptoInfo.publicKeyCryptoSpec = {
+            "sign": "RSA-SHA256"
+        };
+    }
+    cryptoInfo.distCryptoSpec = {
+        "cipher": "AES-128-CBC",
+        "mac": "SHA256"
+    };
+    cryptoInfo.sessionCryptoSpec = {
+        "cipher": "AES-128-CBC",
+        "mac": "SHA256"
+    };
+    if (entityName.toLowerCase().includes('safetycritical')) {
+        cryptoInfo.sessionCryptoSpec.diffieHellman = "secp128r2";
+    }
+    return cryptoInfo;
+}
+
+function getTargetServerInfoList(netId, entityName) {
+    var targetServerInfoList = [];
+    for (var i = 0; i < serverList.length; i++) {
+        var serverInfo = {};
+        serverInfo.name = getNetName(netId) + '.' + serverList[i].name;
+        serverInfo.port = getNetPortBase(netId) + serverList[i].port;
+        serverInfo.host = 'localhost';
+        if (serverList[i].name.toLowerCase().includes('udp')) {
+            if (entityName.toLowerCase().includes('udp')) {
+                targetServerInfoList.push(serverInfo);
+            }
+        }
+        else if (serverList[i].name.toLowerCase().includes('safetycritical')) {
+            if (entityName.toLowerCase().includes('safetycritical')) {
+                targetServerInfoList.push(serverInfo);
+            }
+        }
+        else {
+            if (!entityName.toLowerCase().includes('udp') &&
+                !entityName.toLowerCase().includes('safetycritical')) {
+                    targetServerInfoList.push(serverInfo);
+            }
+
+        }
+    }
+    return targetServerInfoList;
+}
+
+function getListeningServerInfo(netId, server) {
+    var listeningServerInfo = {};
+    listeningServerInfo.host = 'localhost';
+    listeningServerInfo.port = getNetPortBase(netId) + server.port;
+    return listeningServerInfo;
+}
+
+function getEntityConfig(netId, entity) {
+    var entityConfig = {};
+    entityConfig.entityInfo = getEntityInfo(netId, entity.name);
+    entityConfig.authInfo = getAuthInfo(netId, entity.name);
+    entityConfig.cryptoInfo = getCryptoInfo(entity.name);
+    if (entity.name.toLowerCase().includes('client')) {
+        var targetServerInfoList = getTargetServerInfoList(1, entity.name);
+        targetServerInfoList = targetServerInfoList.concat(getTargetServerInfoList(2, entity.name));
+        entityConfig.targetServerInfoList = targetServerInfoList;
+    }
+    if (entity.name.toLowerCase().includes('server')) {
+        entityConfig.listeningServerInfo = getListeningServerInfo(netId, entity);
+    }
+    return entityConfig;
+}
+
+function writeEntityConfigToFile(netId, entityName, entityConfig) {
+    var configFilePath = getNetName(netId) + '/' + entityName + '.config';
+    console.log('Writing entityConfig to ' + configFilePath + ' ...');
+    fs.writeFileSync(configFilePath,
+        JSON2.stringify(entityConfig, null, '\t'),
+        'utf8'
+    );
+}
+
+function generateEntityConfigs(numNets) {
+    for (var netId = 1; netId <= numNets; netId++) {
+        for (var i = 0; i < clientList.length; i++) {
+            if (clientList[i].name == 'ptClient') {
+                continue;
+            }
+            writeEntityConfigToFile(netId, clientList[i].name, getEntityConfig(netId, clientList[i]));
+            //console.log(JSON2.stringify(getEntityConfig(netId, clientList[i]), null, '\t'));
+        }
+        for (var i = 0; i < serverList.length; i++) {
+            if (serverList[i].name == 'ptServer') {
+                continue;
+            }
+            writeEntityConfigToFile(netId, serverList[i].name, getEntityConfig(netId, serverList[i]));
+            //console.log(JSON2.stringify(getEntityConfig(netId, serverList[i]), null, '\t'));
+        }
+    }
+}
+
+var totalNumberOfNets = 2;
+generateEntityConfigs(totalNumberOfNets);
+
+//var strConfig = fs.readFileSync('template.config', 'utf8');
+// var fileLines = fs.readFileSync('template.config', 'utf8').split('\n');
+// var fileString = "";
+// for (var i = 0; i < fileLines.length; i++) {
+//     var line = fileLines[i].trim();
+//     if (line.startsWith('//') || line.length == 0) {
+//         continue;
+//     }
+//     fileString += line;
+// }
+// var jsonConfig = JSON.parse(fileString);
+
 
