9e26d16af127c57fabb40ef8c363d26e9e4a0bea
Hokeun Kim <hokeunkim@eecs.berkeley.edu>
2017-03-06 23:50:09 -0800

    replacing Base64 with Android version Base64 when copying

--- AndroidAuth/init.sh
+++ AndroidAuth/init.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-SESSION_KEY_JAVA_FILE=app/src/main/java/org/iot/auth/crypto/SessionKey.java
-CACHED_SESSION_KEY_TABLE_JAVA_FILE=app/src/main/java/org/iot/auth/db/bean/CachedSessionKeyTable.java
+# Temp file name for sed command
 TEMP_FILE=temp20170306.txt
 
 cp -r ../auth/library/src/main/java/org/iot/auth/* app/src/main/java/org/iot/auth/
 
-
 # To replace String.join with android.text.TextUtils.join
+SESSION_KEY_JAVA_FILE=app/src/main/java/org/iot/auth/crypto/SessionKey.java
+CACHED_SESSION_KEY_TABLE_JAVA_FILE=app/src/main/java/org/iot/auth/db/bean/CachedSessionKeyTable.java
 
 sed 's/String.join/android.text.TextUtils.join/g' $SESSION_KEY_JAVA_FILE > $TEMP_FILE
 mv $TEMP_FILE $SESSION_KEY_JAVA_FILE
 
 sed 's/String.join/android.text.TextUtils.join/g' $CACHED_SESSION_KEY_TABLE_JAVA_FILE > $TEMP_FILE
 mv $TEMP_FILE $CACHED_SESSION_KEY_TABLE_JAVA_FILE
-
 ##
+
+# To replace java.util.Base64 with android.util.Base64
+BUFFER_KEY_JAVA_FILE=app/src/main/java/org/iot/auth/io/Buffer.java
+sed 's/import java.util.Base64;/ /g; s/Base64.getEncoder().encodeToString(bytes)/android.util.Base64.encodeToString(bytes, android.util.Base64.DEFAULT)/g; s/Base64.getDecoder().decode(base64)/android.util.Base64.decode(base64, android.util.Base64.DEFAULT)/g' $BUFFER_KEY_JAVA_FILE > $TEMP_FILE
+mv $TEMP_FILE $BUFFER_KEY_JAVA_FILE
