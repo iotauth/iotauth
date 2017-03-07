05a84fdc85f91875d7144590c67ab10dc64d5648
Hokeun Kim <hokeunkim@eecs.berkeley.edu>
2017-03-06 22:14:57 -0800

    initial work for poritng Auth to Android

---
+++ AndroidAuth/clean.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd app/src/main/java/org/iot/auth/
+rm -rf config crypto db exception io message server util
---
+++ AndroidAuth/init.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp -r ../auth/library/src/main/java/org/iot/auth/* app/src/main/java/org/iot/auth/
+
