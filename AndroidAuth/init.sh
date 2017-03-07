a5ff9c61a3e4cd7ec4941ddaca3016b33d7edde6
Hokeun Kim <hokeunkim@eecs.berkeley.edu>
2017-03-06 23:34:13 -0800

    script for replacing String.join with android.text.TextUtils.join

--- AndroidAuth/init.sh
+++ AndroidAuth/init.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
+SESSION_KEY_JAVA_FILE=app/src/main/java/org/iot/auth/crypto/SessionKey.java
+CACHED_SESSION_KEY_TABLE_JAVA_FILE=app/src/main/java/org/iot/auth/db/bean/CachedSessionKeyTable.java
+TEMP_FILE=temp20170306.txt
+
 cp -r ../auth/library/src/main/java/org/iot/auth/* app/src/main/java/org/iot/auth/
 
+
+# To replace String.join with android.text.TextUtils.join
+
+sed 's/String.join/android.text.TextUtils.join/g' $SESSION_KEY_JAVA_FILE > $TEMP_FILE
+mv $TEMP_FILE $SESSION_KEY_JAVA_FILE
+
+sed 's/String.join/android.text.TextUtils.join/g' $CACHED_SESSION_KEY_TABLE_JAVA_FILE > $TEMP_FILE
+mv $TEMP_FILE $CACHED_SESSION_KEY_TABLE_JAVA_FILE
+
+##
