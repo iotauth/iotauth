01e705838df7270959fa68529afe1b17c8f06035
Hokeun Kim <hokeunkim@eecs.berkeley.edu>
2017-03-24 13:26:11 -0700

    added AuthHeartbeatRespMessage and added missing license information

--- auth/library/src/main/java/org/iot/auth/message/AuthHeartbeatRespMessage.java
+++ auth/library/src/main/java/org/iot/auth/message/AuthHeartbeatRespMessage.java
@@ -13,35 +13,11 @@
  * IOTAUTH_COPYRIGHT_VERSION_1
  */
 
-package org.iot.auth.db;
-
-import java.util.HashMap;
-import java.util.Map;
+package org.iot.auth.message;
 
 /**
- * @author hokeunkim
+ * Created by hokeunkim on 3/24/17.
  */
-public enum AuthDBProtectionMethod {
-    DEBUG(0),
-    ENCRYPT_CREDENTIALS(1),
-    ENCRYPT_ENTIRE_DB(2);
+public class AuthHeartbeatRespMessage {
 
-    AuthDBProtectionMethod(int value) {
-        this.value = value;
-    }
-    public int value() {
-        return value;
-    }
-    public static AuthDBProtectionMethod fromValue(int value) {
-        return typesByValue.get(value);
-    }
-    private static final Map<Integer, AuthDBProtectionMethod> typesByValue =
-            new HashMap<>();
-
-    static {
-        for (AuthDBProtectionMethod type : AuthDBProtectionMethod.values()) {
-            typesByValue.put(type.value, type);
-        }
-    }
-    private final int value;
 }
