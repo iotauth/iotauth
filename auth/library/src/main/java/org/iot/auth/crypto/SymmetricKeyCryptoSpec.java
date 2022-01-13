a3d1be6c1517783f3d1795f28d6f3614fe00c85a
Hokeun Kim <hokeunkim@berkeley.edu>
2022-01-12 17:44:34 -0800

    Fix "Use of a broken or risky cryptographic algorithm" security alert by changing the argument for Cipher.getInstance() from AES/CBC/PKCS5Padding to AES.
    E.g., https://github.com/iotauth/iotauth/security/code-scanning/35?query=ref%3Arefs%2Fheads%2Fmaster

--- auth/library/src/main/java/org/iot/auth/crypto/SymmetricKeyCryptoSpec.java
+++ auth/library/src/main/java/org/iot/auth/crypto/SymmetricKeyCryptoSpec.java
@@ -160,15 +160,15 @@ public class SymmetricKeyCryptoSpec extends CryptoSpec {
         }
         else if (jsCryptoAlgo.equals("AES-128-CBC")) {
             // 128 bits -> 16 bytes
-            return new CryptoAlgoKeySize("AES/CBC/PKCS5Padding", 16);
+            return new CryptoAlgoKeySize("AES", 16);
         }
         else if (jsCryptoAlgo.equals("AES-192-CBC")) {
             // 128 bits -> 16 bytes
-            return new CryptoAlgoKeySize("AES/CBC/PKCS5Padding", 24);
+            return new CryptoAlgoKeySize("AES", 24);
         }
         else if (jsCryptoAlgo.equals("AES-256-CBC")) {
             // 128 bits -> 16 bytes
-            return new CryptoAlgoKeySize("AES/CBC/PKCS5Padding", 32);
+            return new CryptoAlgoKeySize("AES", 32);
         }
         else if (jsCryptoAlgo.equals("SHA256")) {
             return new CryptoAlgoKeySize("HmacSHA256");
