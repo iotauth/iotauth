0cb6c585f0ad045a79c98cfa2fd9e026d939670b
yeongbin7 <baack7700@hanyang.ac.kr>
2022-06-24 15:44:12 +0900

    Implementing load_config function

---
+++ entity/c/client/load_config.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h> 
+
+const char entity_info_sender[] = "entityInfo.name";
+const char entity_info_purpose[] = "entityInfo.purpose";
+const char entity_info_numkey[] = "entityInfo.number_key";
+const char auth_pubkey_path[] = "authInfo.pubkey.path";
+const char entity_info_privkey_path[] = "entityInfo.privkey.path";
+const char authInfo_ip_address[] = "auth.ip.address" ;
+const char authInfo_port[] = "auth.port.number" ;
+const char entity_serverInfo_ip_address[] = "entity.server.ip.address" ;
+const char entity_serverInfo_port[] = "entity.server.port" ;
