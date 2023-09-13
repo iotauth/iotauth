e349e765c0f405ff5ee578383093da624ee21f19
yeongbin <baack7700@hanyang.ac.kr>
2023-09-13 15:12:44 +0900

    revise the code for file system manager

--- examples/filesharing/filesystem_manager.py
+++ examples/filesharing/filesystem_manager.py
@@ -27,10 +27,11 @@ def save_data(recv_data, file_center):
 
 # Concat data to send the information including hash value, sessionkey id, and name to downloader entity.
 def concat_data(recv_data):
-    name_size = recv_data[1] 
+    name_size = recv_data[1]
     name = recv_data[2:2+name_size].decode('utf-8').strip("\x00")
-    res_keyid = file_center["keyid"][0]
-    res_hashvalue = file_center["hash_value"][0]
+    file_index = download_num_check(name)
+    res_keyid = file_center["keyid"][file_index]
+    res_hashvalue = file_center["hash_value"][file_index]
     command = "ipfs cat $1 > "
     command = command.replace("$1", res_hashvalue)
     message = bytearray(3+len(res_keyid)+len(command))
@@ -40,10 +41,20 @@ def concat_data(recv_data):
     message[2+len(res_keyid)] = int(hex(len(command)),16)
     message[3+len(res_keyid):3+len(res_keyid)+len(command)] = bytes.fromhex(str(command).encode('utf-8').hex())
     log_center["name"].append(name), log_center["hash_value"].append(res_hashvalue), log_center["keyid"].append(res_keyid)
+    download_list.append(name)
     return message
 
+def download_num_check(name):
+    num = 0
+    if len(download_list) == 0:
+        return num
+    for i in download_list:
+        if i == name:
+            num += 1
+    return num      
+
 def accept_wrapper(sock):
-    conn, addr = sock.accept()  
+    conn, addr = sock.accept()
     print(f"Accepted connection from {addr}")
     conn.setblocking(False)
     data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
@@ -52,14 +63,14 @@ def accept_wrapper(sock):
 
 file_center = {"name":[] , "keyid" : [], "hash_value" : []}
 log_center = {"name":[] , "keyid" : [], "hash_value" : []}
+download_list = []
 def service_connection(key, mask):
     sock = key.fileobj
     data = key.data
     global payload_max_num
     if mask & selectors.EVENT_READ:
         recv_data = sock.recv(bytes_num)  # Should be ready to read
-        if recv_data:      
-            total_len = len(recv_data)
+        if recv_data:
             print(recv_data)
             if recv_data[0] == DATA_UPLOAD_REQ:
                 save_data(recv_data, file_center)
@@ -69,14 +80,14 @@ def service_connection(key, mask):
                 data.outb += message
                 sent = sock.send(data.outb) 
                 data.outb = data.outb[sent:]
-                del file_center["hash_value"][0], file_center["keyid"][0], file_center["name"][0]
+                # del file_center["hash_value"][0], file_center["keyid"][0], file_center["name"][0]
         else:
             print(f"Closing connection to {data.addr}")
             sel.unregister(sock)
 
     
-port = 22100
-host, port = '127.0.0.1', port
+port = 7001
+host, port = '0.0.0.0', port
 
 lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 lsock.bind((host, port))
